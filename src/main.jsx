337db5792847221c05f0c45fcb6f858a359c1ce5
Angel Avendaño Cruz <angel.aven.c@gmail.com>
2025-03-14 02:42:55 -0600

    The Root Route
    Let's add the global layout for this app.

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import {Root} from './routes/root.jsx'
 import './index.css'
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hello World!</div>
-    }
+        element: <Root/>
+    },
 ])
 
 createRoot(document.getElementById('root')).render(
